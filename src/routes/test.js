94b4ef2fb4727bea541d21e3bf0eb1c903864aee
gaiceson <gaiceson@gmail.com>
2022-10-07 15:25:58 +0900

    test code running 1

--- src/routes/test.js
+++ src/routes/test.js
@@ -1,1 +1,13 @@
-ReadableByteStreamController
+import { useEffect, useState } from "react";
+
+const Test = () => {
+    return (
+        <>
+        <h4>타이틀</h4>
+        <div>Hello GitTest!!!222!</div>
+        </>
+
+    );
+}
+
+export default Test;
